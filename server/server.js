c2263e01243cfc4b75f15cef43e92853ee23edff
Alvin Graham <grah0071@gmail.com>
2024-02-21 21:57:00 -0600

    Add server side giphy API endpoint GET route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const favoriteRouter = require('./routes/favorite.router');
 const categoryRouter = require('./routes/category.router');
+const searchRouter = require('./routes/search.router');
+
 const PORT = process.env.PORT || 5001;
 
 /** ---------- MIDDLEWARE ---------- **/
@@ -12,6 +15,7 @@ app.use(express.static('build'));
 /** ---------- EXPRESS ROUTES ---------- **/
 app.use('/api/favorites', favoriteRouter);
 app.use('/api/categories', categoryRouter);
+app.use('/api/search', searchRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
